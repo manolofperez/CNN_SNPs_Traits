cb7d81f6528aa87b5e3ee52d21964dea76306648
Manolo Perez <manolo@macbook-pro-de-manolo.home>
2020-05-28 23:21:09 +0200

    remove sort

--- simulate_ms_SNP_trees_CNN.py
+++ simulate_ms_SNP_trees_CNN.py
@@ -10,15 +10,6 @@ import math
 import shlex, subprocess
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
-
-def sort_min_diff(amat):
-    '''this function takes in a SNP matrix with indv on rows and returns the same matrix with indvs sorted by genetic similarity.
-    this problem is NP-hard, so here we use a nearest neighbors approx.  it's not perfect, but it's fast and generally performs ok.
-    assumes your input matrix is a numpy array'''
-    mb = NearestNeighbors(len(amat), metric='manhattan').fit(amat)
-    v = mb.kneighbors(amat)
-    smallest = np.argmin(v[0].sum(axis=1))
-    return amat[v[1][smallest]]
     
 def ms2nparray(xfile):
 	g = list(xfile)
