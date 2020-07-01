05b84ffdd785d23c1e714a72403c8b650a95ec32
Manolo Perez <manolo@macbook-pro-de-manolo-1.home>
2020-07-01 04:21:25 +0200

    updt

--- simulate_ms_SNP_trees_CNN.py
+++ simulate_ms_SNP_trees_CNN.py
@@ -15,7 +15,7 @@ def ms2nparray(xfile):
 	k = [idx for idx,i in enumerate(g) if len(i) > 0 and i.startswith(b'//')]
 	f = []
 	for i in k:
-		L = g[i+5:i+nDNANsam+5]
+		L = g[i+5:i+N_allpops+5]
 		q = []
 		for i in L:
 			i = [int(j-48) for j in list(i)]
@@ -39,40 +39,24 @@ def get_newick(xfile):
 #define the number of simulations
 Priorsize = 1000
 
-## nDNA sample size of JFE.
-nDNAJFE = 8
+## sample size of pop1.
+N_popA = 10
 ## nDNA sample size of ITA.
-nDNAITA = 8
+N_popB = 10
 ## nDNA sample size of PMN.
-nDNAPMN = 8
-## nDNA sample size of EDB.
-nDNAEDB = 8
-## nDNA sample size of BOV.
-nDNABOV = 8
-## nDNA sample size of COC.
-nDNACOC = 8
-## nDNA sample size of INA.
-nDNAINA = 8
-## nDNA sample size of ODA.
-nDNAODA = 8
-## nDNA sample size of MEN.
-nDNAMEN = 8
+N_popC = 10
 
-## nDNA sample size of ITAPMN.
-nDNAITAPMN = nDNAITA + nDNAPMN
-## nDNA sample size of BOVEDB.
-nDNABOVEDB = nDNAEDB + nDNABOV
-## nDNA sample size of Central.
-nDNACentral = nDNACOC + nDNAINA + nDNAODA + nDNAMEN
+## sample size for pops 1 and 2 combined.
+N_popAB = N_popA + N_popB
 
-## nDNA sample sizes (number of alleles).
-nDNANsam = nDNAITA + nDNAPMN + nDNAEDB + nDNABOV + nDNAJFE + nDNACOC + nDNAINA + nDNAODA + nDNAMEN
-## number of years per generation
-genlen = 15
+## sample size for all pops combined.
+N_allpops = N_popA + N_popB + N_popC
 
 simModel1 = []
 simModel2 = []
 simModel3 = []
+simModel4 = []
+
 ## create a file to store parameters and one to store the models
 parameters = open("parameters.txt","w")
 models = open("models.txt","w")
@@ -80,22 +64,22 @@ trees = open("trees.txt","w")
 os.mkdir("trainingSims")
 
 #Define default values for priors absent in some models.
-T5=0
-T6=0
+T2=0
+T1=0
+M=0
 
-### Clade ES Geneland Model
+### One species
 for i in range(Priorsize):
 	### Define parameters
 	## Theta values from 1 to 15
-	Theta = random.uniform(1,15)
-	## divergence time prior set to 0 in this model.
-	coalRootDivTime = 0
-	## nDNA ms's command
-	com=subprocess.Popen("./ms %d 500 -s 1 -t %f -I 4 %d %d %d %d -ej 0 2 4 -ej 0 3 4 -ej 0 1 4 -T" % (nDNANsam, Theta, nDNAJFE, nDNAITAPMN ,nDNABOVEDB , nDNACentral), shell=True, stdout=subprocess.PIPE).stdout
+	Theta = random.uniform(1,10)
+
+	## ms command
+	com=subprocess.Popen("./ms %d 1000 -s 1 -t %f -T" % (N_allpops, Theta), shell=True, stdout=subprocess.PIPE).stdout
 	output = com.read().splitlines()
-	simModel1.append(np.array(ms2nparray(output)).swapaxes(0,1).reshape(nDNANsam,-1).T)
+	simModel1.append(np.array(ms2nparray(output)).swapaxes(0,1).reshape(N_allpops,-1).T)
 	## save parameter values and models
-	parameters.write("%f\t%f\n" % (Theta, coalRootDivTime))
+	parameters.write("%f\t%f\t%f\t%f\n" % (Theta, T2, T1, M))
 	models.write("1\n")
 	trees.write("%s\n" % (get_newick(output).decode('utf-8')))
 	print("Completed %d %% of Model 1 simulations" % (float(i)/Priorsize*100))
@@ -105,22 +89,21 @@ simModel1=np.array(simModel1)
 np.savez_compressed('trainingSims/simModel1.npz', simModel1=simModel1)
 del(simModel1)
 
-### Clade ES Splitter Model
+### Two species, A and B collapsed
 for i in range(Priorsize):
 
 	## Theta values from 1 to 15
-	Theta = random.uniform(1,15)
+	Theta = random.uniform(1,10)
 	## divergence time prior following an uniform distribution from 0.5 to 5.
-	coalRootDivTime = random.uniform(0.5,5)
-	coalT1=random.uniform(0,coalRootDivTime)
-	coalT2=random.uniform(0,coalT1)
-	## nDNA ms's command
-	com=subprocess.Popen("./ms %d 500 -s 1 -t %f -I 4 %d %d %d %d -ej %f 2 4 -ej %f 3 4 -ej %f 1 4 -T" % (nDNANsam, Theta, nDNAJFE, nDNAITAPMN ,nDNABOVEDB , nDNACentral, coalT2, coalT1, coalRootDivTime), shell=True, stdout=subprocess.PIPE).stdout
+	T2 = random.uniform(2,5)
+
+	## ms command
+	com=subprocess.Popen("./ms %d 1000 -s 1 -t %f -I 2 %d %d -ej %f 1 2 -T" % (N_allpops, Theta, N_popAB, N_popC ,T2), shell=True, stdout=subprocess.PIPE).stdout
 	output = com.read().splitlines()
-	simModel2.append(np.array(ms2nparray(output)).swapaxes(0,1).reshape(nDNANsam,-1).T)
+	simModel2.append(np.array(ms2nparray(output)).swapaxes(0,1).reshape(N_allpops,-1).T)
 
 	## save parameter values and models
-	parameters.write("%f\t%f\n" % (Theta, coalRootDivTime))
+	parameters.write("%f\t%f\t%f\t%f\n" % (Theta, T2, T1, M))
 	models.write("2\n")
 	trees.write("%s\n" % (get_newick(output).decode('utf-8')))
 	print("Completed %d %% of Model 2 simulations" % (float(i)/Priorsize*100))
@@ -132,23 +115,48 @@ np.savez_compressed('trainingSims/simModel2.npz', simModel2=simModel2)
 del(simModel2)
 
 
-### Clade ES Lumper Model
+### Three species, isolation only
 for i in range(Priorsize):
 
 	## Theta values from 1 to 15
-	Theta = random.uniform(1,15)
+	Theta = random.uniform(1,10)
 	## divergence time prior following an uniform distribution from 0.5 to 5.
-	coalRootDivTime = random.uniform(0.5,5)
+	T2 = random.uniform(2,5)
+	T1 = random.uniform(0.5,T2)
 
 	## nDNA ms's command
-	com=subprocess.Popen("./ms %d 500 -s 1 -t %f -I 4 %d %d %d %d -ej 0 2 4 -ej 0 3 4 -ej %f 1 4 -T" % (nDNANsam, Theta, nDNAJFE, nDNAITAPMN ,nDNABOVEDB , nDNACentral, coalRootDivTime), shell=True, stdout=subprocess.PIPE).stdout
+	com=subprocess.Popen("./ms %d 1000 -s 1 -t %f -I 3 %d %d %d -ej %f 1 2 -ej %f 3 2 -T" % (N_allpops, Theta, N_popA, N_popB ,N_popC, T1, T2), shell=True, stdout=subprocess.PIPE).stdout
 	output = com.read().splitlines()
-	simModel3.append(np.array(ms2nparray(output)).swapaxes(0,1).reshape(nDNANsam,-1).T)
+	simModel3.append(np.array(ms2nparray(output)).swapaxes(0,1).reshape(N_allpops,-1).T)
 	## save parameter values
-	parameters.write("%f\t%f\n" % (Theta, coalRootDivTime))
+	parameters.write("%f\t%f\t%f\t%f\n" % (Theta, T2, T1, M))
 	models.write("3\n")
 	trees.write("%s\n" % (get_newick(output).decode('utf-8')))
 	print("Completed %d %% of Model 3 simulations" % (float(i)/Priorsize*100))
 
 simModel3=np.array(simModel3)
 np.savez_compressed('trainingSims/simModel3.npz', simModel3=simModel3)
+
+### Three species, isolation with migration
+for i in range(Priorsize):
+
+	## Theta values from 1 to 15
+	Theta = random.uniform(1,10)
+	## divergence time prior following an uniform distribution from 0.5 to 5.
+	T2 = random.uniform(2,5)
+	T1 = random.uniform(0.5,T2)
+	M = random.uniform(1,5)
+
+	## nDNA ms's command
+	com=subprocess.Popen("./ms %d 1000 -s 1 -t %f -I 3 %d %d %d %f -ej %f 1 2 -ej %f 3 2 -T" % (N_allpops, Theta, N_popA, N_popB ,N_popC, M, T1, T2), shell=True, stdout=subprocess.PIPE).stdout
+	output = com.read().splitlines()
+	simModel4.append(np.array(ms2nparray(output)).swapaxes(0,1).reshape(N_allpops,-1).T)
+	## save parameter values
+	parameters.write("%f\t%f\t%f\t%f\n" % (Theta, T2, T1, M))
+	models.write("4\n")
+	trees.write("%s\n" % (get_newick(output).decode('utf-8')))
+	print("Completed %d %% of Model 4 simulations" % (float(i)/Priorsize*100))
+
+simModel3=np.array(simModel3)
+np.savez_compressed('trainingSims/simModel4.npz', simModel4=simModel4)
+
